9f1d2c8baa161b0984127f6ddc94c2fd2ff2ef18
dillajetmiko <dilla.jetmiko@gmail.com>
2021-07-18 01:10:56 +0700

    modifikasi tabel view cetak dan user: menambahkan data

--- resources/views/cetak/cetak.blade.php
+++ resources/views/cetak/cetak.blade.php
@@ -1,8 +1,8 @@
 @extends("layout.mainlayout")
 
-@section("page_title","Cetak")
+@section("page_title","CETAK")
 
-@section("title","Cetak")
+@section("title","CETAK")
 
 @section("breadcrumb")
 <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
@@ -16,79 +16,84 @@
 @endsection
 
 @section('content')
-
 <!-- Default box -->
 <div class="card">    
-	<div class="card-header">
-		<h3 class="card-title"></h3>
-
-		<div class="card-tools">
-			<button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-			  <i class="fas fa-minus"></i></button>
-			<button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-			  <i class="fas fa-times"></i></button>
-		</div>
-	</div>
-	<div class="card-body">
+  <div class="card-header">
+	  <h3 class="card-title"> DATA CETAK</h3>
+	  <div class="card-tools">
+		  <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+			<i class="fas fa-minus"></i></button>
+		  <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+			<i class="fas fa-times"></i></button>
+	  </div>
+  </div>
+  <div class="card-body">
 		<div class="card">
-			  <div class="card-header">
-				<!-- <h3 class="card-title">Tambah Data Anggota</h3> -->
-				<a href="/temuan/insert_temuan">
-				<!-- <button type="button" class="btn btn-info float-right" style="float: right;"><i class="fas fa-plus"></i>  Tambah Data Temuan</button> -->
-				</a>
-			  </div>
-			  <!-- /.card-header -->
-			  <div class="card-body">
-				Judul Pemerikasaan<br>
+			<!-- <div class="card-header">
+			<h3 class="card-title">Tambah Data Anggota</h3>
+			</div> -->
+			<!-- /.card-header -->
+			<div class="card-body">
+				Judul Pemeriksaan<br>
 				Jenis Pengawasan<br>
 				Tanggal LHP<br>
-				Tanggal Temuan<br>
-				<br>
-				<table id="example1" class="table table-bordered table-striped">
-				  <thead>
-				  
-					<tr>		
-						<th colspan="2" scope="colgroup" style="text-align:center">Temuan</th>
-						<th colspan="2" scope="colgroup" style="text-align:center">Rekomendasi</th>
-						<th colspan="2" scope="colgroup" style="text-align:center">Tindak Lanjut</th>
-						<th rowspan="2" scope="rowgroup" style="text-align:center">Kerugian</th>
-						<th rowspan="2" scope="rowgroup" style="text-align:center">Nama OPD</th>
-					</tr>
-					<tr>
-						<th style="text-align:center">Kode</th>
-						<th style="text-align:center">Uraian</th>
-						<th style="text-align:center">Kode</th>
-						<th style="text-align:center">Uraian</th>
-						<th style="text-align:center">Uraian</th>
-						<th style="text-align:center">Status</th>
-				  </tr>
-				  </thead>
-				  <tbody>
-				  
-
-
-				  </tbody>
-				  <tfoot>
-				  <!-- <tr>
-				  <th>NIS_NIP</th>
-				  <th>nama_anggota</th>
-				  <th>tahun_masuk</th>
-				  <th>kelas</th>
-				  <th>username_anggota</th>
-				  <th>password_anggota</th>
-				  </tr> -->
-				  </tfoot>
-				</table>
-			  </div>
-			  <!-- /.card-body -->
+			<table id="example1" class="table table-bordered table-striped">
+        <thead>
+          <tr>		
+            <th rowspan="2" scope="rowgroup" style="text-align:center">Nomer LHP</th>
+            <th colspan="2" scope="colgroup" style="text-align:center">Temuan</th>
+            <th colspan="2" scope="colgroup" style="text-align:center">Rekomendasi</th>
+            <th colspan="2" scope="colgroup" style="text-align:center">Tindak Lanjut</th>
+            <th rowspan="2" scope="rowgroup" style="text-align:center">Tanggal Tindak Lanjut</th>
+            <th rowspan="2" scope="rowgroup" style="text-align:center">Kerugian</th>
+            <th rowspan="2" scope="rowgroup" style="text-align:center">Nama OPD</th>
+          </tr>
+          <tr>
+            <th style="text-align:center">Kode</th>
+            <th style="text-align:center">Uraian</th>
+            <th style="text-align:center">Kode</th>
+            <th style="text-align:center">Uraian</th>
+            <th style="text-align:center">Uraian</th>
+            <th style="text-align:center">Status</th>
+          </tr>
+        </thead>
+				<tbody>		
+        @foreach($cetak as $data)
+				<tr>
+					<td>{{ $data->KODE_TEMUAN }}</td>
+					<td>{{ $data->NOMOR_LHP }}</td>
+					<td>{{ $data->URAIAN_TEMUAN }}</td>
+					<td>{{ $data->KODE_REKOMENDASI }}</td>
+					<td>{{ $data->URAIAN_REKOMENDASI }}</td>
+					<td>{{ $data->URAIAN_TINDAK_LANJUT }}</td>
+					<td>{{ $data->KODE_STATUS }}</td>
+					<td>{{ $data->TANGGAL_TINDAK_LANJUT }}</td>
+					<td>{{ $data->KERUGIAN }}</td>
+					<td>{{ $data->KODE_OPD }}</td>
+				</tr>
+				@endforeach
+				</tbody>
+				<tfoot>
+				<!-- <tr>
+				<th>NIS_NIP</th>
+				<th>nama_anggota</th>
+				<th>tahun_masuk</th>
+				<th>kelas</th>
+				<th>username_anggota</th>
+				<th>password_anggota</th>
+				</tr> -->
+				</tfoot>
+			</table>
 			</div>
-			<!-- /.card -->
+			<!-- /.card-body -->
 		</div>
-		<!-- /.card-body -->
-		<div class="card-footer">
-			<!-- <a href="/LHP/insert_lhp"><b>Tambah Data LHP</b></a> -->
-		</div>
-		<!-- /.card-footer-->
+		<!-- /.card -->
+  </div>
+  <!-- /.card-body -->
+  <div class="card-footer">
+	<!-- <a href="/LHP/insert_lhp"><b>Tambah Data LHP</b></a> -->
+  </div>
+  <!-- /.card-footer-->
 </div>
 <!-- /.card -->
 @endsection
--- resources/views/user/view_user.blade.php
+++ resources/views/user/view_user.blade.php
@@ -1,88 +1,92 @@
 @extends("layout.mainlayout")
- @section("page_title","USER")
 
- @section("custom_css")
- <style>
- div.c {
-  text-align: right;
-} 
- </style>
- @endsection
+@section("page_title","USER")
 
- @section("title","USER")
+@section("title","USER")
 
- @section("breadcrumb")
- <li class="breadcrumb-item"><a href="#">Home</a></li>
- <li class="breadcrumb-item active">Blank Page</li> 
- @endsection
+@section("breadcrumb")
+<li class="breadcrumb-item"><a href="dashboard">Home</a></li>
+<li class="breadcrumb-item active">User</li> 
+@endsection
 
- @section('custom_css')
+@section('custom_css')
 <!-- DataTables -->
 <link rel="stylesheet" href="{{asset ('asset/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
 <link rel="stylesheet" href="{{asset ('asset/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
 @endsection
 
 @section('content')
- <!-- Default box -->
- <div class="card">    
-    <div class="card-header">
-        <h3 class="card-title"> DATA USER</h3>
-
-        <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-              <i class="fas fa-minus"></i></button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fas fa-times"></i></button>
-        </div>
-    </div>
-    <div class="card-body">
-    <div class="card">
-              <div class="card-header">
-                <!-- <h3 class="card-title">Tambah Data Anggota</h3> -->
-                <a href="/user/insert_user">
-                <button type="button" class="btn btn-info float-right" style="float: right;"><i class="fas fa-plus"></i>  Tambah Data User</button>
-                </a>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
-                  <thead>
-                  <tr>
-                  <th style="text-align:center">NIP</th>
-                  <th style="text-align:center">Nama</th>
-                  <th style="text-align:center">Jabatan</th>
-                  <th style="text-align:center">Pangkat</th>
-                  <th style="text-align:center">Action</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  
-
-
-                  </tbody>
-                  <tfoot>
-                  <!-- <tr>
-                  <th>NIS_NIP</th>
-                  <th>nama_anggota</th>
-                  <th>tahun_masuk</th>
-                  <th>kelas</th>
-                  <th>username_anggota</th>
-                  <th>password_anggota</th>
-                  </tr> -->
-                  </tfoot>
-                </table>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-  <!-- <a href="/LHP/insert_lhp"><b>Tambah Data LHP</b></a> -->
-        </div>
-        <!-- /.card-footer-->
+<!-- Default box -->
+<div class="card">    
+  <div class="card-header">
+	  <h3 class="card-title"> DATA USER</h3>
+	  <div class="card-tools">
+		  <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+			<i class="fas fa-minus"></i></button>
+		  <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+			<i class="fas fa-times"></i></button>
+	  </div>
+  </div>
+  <div class="card-body">
+		<div class="card">
+			<div class="card-header">
+			<!-- <h3 class="card-title">Tambah Data Anggota</h3> -->
+			<a href="/user/insert_user">
+			<button type="button" class="btn btn-info float-right" style="float: right;"><i class="fas fa-plus"></i>  Tambah Data User</button>
+			</a>
+			</div>
+			<!-- /.card-header -->
+			<div class="card-body">
+			<table id="example1" class="table table-bordered table-striped">
+				<thead>
+				<tr>
+				<th style="text-align:center">NIP</th>
+				<th style="text-align:center">NAMA</th>
+				<th style="text-align:center">JABATAN</th>
+				<th style="text-align:center">PANGKAT</th>
+				<th style="text-align:center">Aksi</th>
+				</tr>
+				</thead>
+				<tbody>
+				@foreach($user as $data)
+				<tr>
+					<td>{{ $data->NIP }}</td>
+					<td>{{ $data->NAMA }}</td>
+					<td>{{ $data->JABATAN }}</td>
+					<td>{{ $data->PANGKAT }}</td>
+					<td><a href='/user/edit_user/{{ $data->NIP }}'>
+					<button> Edit </button>
+					</a>
+					<a href='/user/hapus/{{ $data->NIP }}'>
+					<button> Hapus </button> 
+					</a>
+					</td>             
+				</tr>
+				@endforeach
+				</tbody>
+				<tfoot>
+				<!-- <tr>
+				<th>NIS_NIP</th>
+				<th>nama_anggota</th>
+				<th>tahun_masuk</th>
+				<th>kelas</th>
+				<th>username_anggota</th>
+				<th>password_anggota</th>
+				</tr> -->
+				</tfoot>
+			</table>
+			</div>
+			<!-- /.card-body -->
+		</div>
+		<!-- /.card -->
+  </div>
+  <!-- /.card-body -->
+  <div class="card-footer">
+	<!-- <a href="/LHP/insert_lhp"><b>Tambah Data LHP</b></a> -->
+  </div>
+  <!-- /.card-footer-->
 </div>
-      <!-- /.card -->
+<!-- /.card -->
 @endsection
 
 
@@ -95,10 +99,10 @@
 
 <script>
   $(function () {
-    $("#example1").DataTable({
-      "responsive": true,
-      "autoWidth": false,
-    });
+	$("#example1").DataTable({
+	  "responsive": true,
+	  "autoWidth": false,
+	});
   });
 </script>
 @endsection
