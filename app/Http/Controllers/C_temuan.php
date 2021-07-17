145de1fc8a5748943a8672241dc699387008497a
slestarii <87431672+slestarii@users.noreply.github.com>
2021-07-17 16:08:28 +0700

    Update C_temuan.php

--- app/Http/Controllers/C_temuan.php
+++ app/Http/Controllers/C_temuan.php
@@ -22,12 +22,25 @@ class C_temuan extends Controller
 
     public function insertTemuan()
     {
+        $temuan = DB::table('temuan')->get();
         $data = array(
             'menu' => 'temuan',
             'temuan' => $temuan,
             'submenu' => ''
         );
-        return view('temuan/insert_temuan');
-        //return view('tambah_bahasa');     
+        return view('temuan/insert_temuan',$data);
     }
+    // public function tambahTemuan(Request $post)
+    // {
+    //     DB::table('temuan')->insert([
+    //         'NOMOR_LHP' => $post->NOMOR_LHP,
+    //         'NIP' => '1',
+    //         'TANGGAL_LHP' => $post->TANGGAL_LHP,
+    //         'JUDUL_PEMERIKSAAN' => $post->JUDUL_PEMERIKSAAN,
+    //         'ANGGARAN' => $post->ANGGARAN,
+    //     ]);
+
+    //     return redirect('/temuan');
+    // }
+    
 }
