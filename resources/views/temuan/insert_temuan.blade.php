9d59d360f585bcd90447563e167ea91bfe2ad692
slestarii <87431672+slestarii@users.noreply.github.com>
2021-07-17 16:08:33 +0700

    Update insert_temuan.blade.php

--- resources/views/temuan/insert_temuan.blade.php
+++ resources/views/temuan/insert_temuan.blade.php
@@ -28,7 +28,8 @@
     </div>
     <div class="card-body">
     <!-- <h1>Tambah Data Temuan</h1> -->
- <form action="/anggota/insertData" method="post">
+      
+ <form action="/temuan/tambah_temuan" method="post">
  <input type = "hidden" name = "_token" value = "<?php echo csrf_token() ?>">
  Kode Temuan : <input type="text" class="form-control" name="kode_temuan"><br>
  Uraian Temuan : <input type="text" class="form-control" name="uraian_temuan"><br>
@@ -39,7 +40,6 @@
  Tanggal Tindak Lanjut : <input type="date" class="form-control" name="tanggal_tindak_lanjut"><br>
  Nomor LHP : <input type="text" class="form-control" name="nomor_lhp"><br>
  Kerugian : <input type="text" class="form-control" name="kerugian"><br>
- <!-- Status Anggota : <input type="tinyint" class="form-control" name="status_anggota"><br> -->
  Status :<br> 
             <label><input type="radio" name="status" value="0" checked="checked" /> Belum Ditindak Lanjut </label><br>
             <label><input type="radio" name="status" value="1" /> Belum Sesuai Rekomendasi </label><br>
