5c44f79a5d792300c1f61a1fab40d40691b712b1
slestarii <87431672+slestarii@users.noreply.github.com>
2021-07-17 22:59:06 +0700

    Update C_temuan.php

--- app/Http/Controllers/C_temuan.php
+++ app/Http/Controllers/C_temuan.php
@@ -30,4 +30,27 @@ class C_temuan extends Controller
         return view('temuan/insert_temuan');
         //return view('tambah_bahasa');     
     }
+<<<<<<< Updated upstream
+=======
+
+    public function tambahTemuan(Request $post)
+    {
+        DB::table('temuan')->insert([
+            'kode_temuan' => $post->kode_temuan,
+            'uraian_temuan' => $post->uraian_temuan,
+            'rekomendasi' => $post->rekomendasi,
+            'nama_opd' => $post->nama_opd,
+            'nama_pejabat' => $post->nama_pejabat,
+            'tanggal_temuan' => $post->tanggal_temuan,
+            'tanggal_tindak_lanjut' => $post->tanggal_tindak_lanjut,
+            'nomor_lhp' => $post->nomor_lhp,
+            'kerugian' => $post->kerugian,
+            'status' => $post->status,
+            'hasil_telaah' => $post->hasil_telaah,
+            'kode_jenis_temuan' => $post->kode_jenis_temuan
+        ]);
+
+        return redirect('/temuan');
+    }   
+>>>>>>> Stashed changes
 }
