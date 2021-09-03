4aba6a7b207f2f72843b1e90ec5f6a987d57a026
dillajetmiko <dilla.jetmiko@gmail.com>
2021-09-03 13:00:22 +0700

    modifikasi edit temuan textarea

--- resources/views/temuan/edit_temuan.blade.php
+++ resources/views/temuan/edit_temuan.blade.php
@@ -57,7 +57,9 @@
                 </select>
                 <br>
 
-            Uraian Temuan : <input type="text" class="form-control" name="URAIAN_TEMUAN" value="{{ $temuan[0]->URAIAN_TEMUAN }}"><br>
+          Uraian Temuan : 
+          <textarea type="text" class="form-control" name="URAIAN_TEMUAN">{{ $temuan[0]->URAIAN_TEMUAN }}</textarea><br>
+          
         	Kerugian: <input type="text" class="form-control" name="KERUGIAN" value="{{ $temuan[0]->KERUGIAN}}"><br>
 
             Jenis Temuan : 
