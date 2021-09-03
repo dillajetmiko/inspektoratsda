e1e32f29186e1001015b2ab6371b498a010dc62d
dillajetmiko <dilla.jetmiko@gmail.com>
2021-09-03 09:29:24 +0700

    hapus spt

--- app/Http/Controllers/C_spt.php
+++ app/Http/Controllers/C_spt.php
@@ -401,8 +401,8 @@ class C_spt extends Controller
  
      public function hapus($ID_SPT)
      {
-         DB::table('penugasan')->where('id',$ID_SPT)->delete();
-         DB::table('dasar')->where('id',$ID_SPT)->delete();
+         DB::table('penugasan')->where('id_spt',$ID_SPT)->delete();
+         DB::table('dasar')->where('id_spt',$ID_SPT)->delete();
          DB::table('spt')->where('id',$ID_SPT)->delete();
          return redirect('/spt');
      }
